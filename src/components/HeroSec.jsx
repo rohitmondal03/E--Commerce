3369e6c3e4ae469e2e42a0fa5b52bb56ea644a73
Rohit <rohitmondall8000@gmail.com>
2023-06-04 21:07:47 +0530

    working on carousel section

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 
+
+// ASSETS
 import nikeImg from "../assets/nike-shoes.jpg"
+
+
+// ANT DESIGN
+import { Carousel } from 'antd';
+
+
+
+const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
 
 
 const HeroSec = () => {
     return (
         <section className=''>
-            <div className='flex flex-row items-center justify-between'>
-                <div className='mr-32'>
+            <div className=''>
+                <div className='text-center'>
                     <h1
                         className='text-6xl font-extrabold mb-8'
                         style={{
@@ -19,18 +35,38 @@ const HeroSec = () => {
                     <p className='text-xl font-semibold text-zinc-500'>- your ultimate destination for online shopping! At E-Com, we believe that shopping should be an enjoyable and convenient experience. With our wide range of products, seamless user interface, and top-notch customer service, we strive to make your online shopping journey as effortless as possible.</p>
                 </div>
 
-                <img
+                {/* <img
                     src={nikeImg}
                     className='h-96'
                     alt='E-Com intro img'
-                />
-            </div>
+                /> */}
 
-            <p
-                className='text-2xl text-center text-zinc-600 w-4/5 mx-auto font-bold my-16'
-                // style={{textShadow: '0px 0px 150px grey'}}
-            >
-                Discover a vast collection of products carefully curated to cater to your diverse needs and preferences. Whether you're looking for fashion essentials to upgrade your wardrobe, electronics to enhance your digital lifestyle, home decor to transform your living space, or beauty and wellness products to pamper yourself, we have it all. Our extensive selection ensures that you can find exactly what you're searching for, all in one place.</p>
+                <div className='mx-auto w-1/2'>
+                    <Carousel autoplay className=''>
+                        <div className='mx-auto'>
+                            <img
+                                src={nikeImg}
+                                className='mx-auto h-96'
+                            />
+                        </div>
+                        <div>
+                            <img
+                                src={nikeImg}
+                            />
+                        </div>
+                        <div>
+                            <img
+                                src={nikeImg}
+                            />
+                        </div>
+                        <div>
+                            <img
+                                src={nikeImg}
+                            />
+                        </div>
+                    </Carousel>
+                </div>
+            </div>
         </section>
     )
 }
